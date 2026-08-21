0ba96ff12e3c47b27d704cc58a9a55d2cde1472e
Ben <benmega@gmail.com>
2026-08-21 11:17:31 +0700

    Remove unneeded temporary scripts and HTML files

--- remove_inline.py
+++
@@ -1,8 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace(' style="filter:invert(1);"', '')
-
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(content)
-print('Done!')
